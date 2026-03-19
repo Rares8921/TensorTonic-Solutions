def7c971519f434875c8195338d74a2077aed5e3
Rares Cocosila-Dumitriu <59397355+Rares8921@users.noreply.github.com>
2026-03-19 14:36:36 +0200

    Update solution for anchor-box-generation

--- anchor-box-generation/anchor-box-generation.py
+++ anchor-box-generation/anchor-box-generation.py
@@ -1,22 +1,27 @@
-from math import sqrt
+import numpy as np
 
 def generate_anchors(feature_size, image_size, scales, aspect_ratios):
     """
     Generate anchor boxes for object detection.
     """
-    # Write code here
     stride = image_size / feature_size
-    anchors = []
-    for i in range(feature_size):
-        for j in range(feature_size):
-            cx, cy = (j + 0.5) * stride, (i + 0.5) * stride
-            
-            for s in scales:
-                for r in aspect_ratios:
-                    w = s * sqrt(r)
-                    h = s / sqrt(r)
+    centers = (np.arange(feature_size) + 0.5) * stride
+    cx, cy = np.meshgrid(centers, centers) # shape (feature_size, feature_size)
 
-                    anchor = [cx - w / 2, cy - h / 2, cx + w / 2, cy + h / 2]
-                    anchors.append(anchor)
+    cx = cx.flatten()[:, None, None] # shape (num_cells, 1, 1)
+    cy = cy.flatten()[:, None, None] # shape (num_cells, 1, 1)
 
-    return anchors
+    scales = np.array(scales)[None, :, None] # shape (1, num_scales, 1)
+    ratios = np.array(aspect_ratios)[None, None, :] # shape (1, 1, num_ratios)
+
+    w = scales * np.sqrt(ratios) # shape (1, num_scales, num_ratios)
+    h = scales / np.sqrt(ratios)
+
+    anchors = np.stack([
+        cx - w / 2, # x_min
+        cy - h / 2, # y_min
+        cx + w / 2, # x_max
+        cy + h / 2  # y_max
+    ], axis = -1) # shape (num_cells, num_scales, num_ratios, 4)
+
+    return anchors.reshape(-1, 4).tolist()

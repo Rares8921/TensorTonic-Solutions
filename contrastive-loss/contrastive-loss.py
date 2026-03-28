c8f62fe556796142d6c0395fa8da756dd78ab3c2
Rares Cocosila-Dumitriu <59397355+Rares8921@users.noreply.github.com>
2026-03-28 12:14:19 +0200

    Add solution for contrastive-loss

---
+++ contrastive-loss/contrastive-loss.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def contrastive_loss(a, b, y, margin=1.0, reduction="mean") -> float:
+    """
+    a, b: arrays of shape (N, D) or (D,)  (will broadcast to (N,D))
+    y:    array of shape (N,) with values in {0,1}; 1=similar, 0=dissimilar
+    margin: float > 0
+    reduction: "mean" (default) or "sum"
+    Return: float
+    """
+    # Write code here
+    a = np.asarray(a, dtype=float)
+    b = np.asarray(b, dtype=float)
+    y = np.asarray(y, dtype=float)
+
+    d = np.linalg.norm(a - b, axis=(a.ndim-1))
+
+    l = y * d**2 + (1 - y) * np.maximum(0, margin - d)**2
+
+    if reduction == "mean":
+        return np.mean(l)
+
+    return np.sum(l)

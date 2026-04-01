4ee315ee1b7b8b262602784baaa9dd1945aca6f6
Rares Cocosila-Dumitriu <59397355+Rares8921@users.noreply.github.com>
2026-04-01 15:20:38 +0300

    Add solution for covariance-matrix

---
+++ covariance-matrix/covariance-matrix.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def covariance_matrix(X):
+    """
+    Compute covariance matrix from dataset X.
+    """
+    X = np.asarray(X)
+
+    if X.ndim != 2 or X.shape[0] < 2:
+        return None
+
+    N = X.shape[0]
+    mu = np.mean(X, axis=0)
+    X_centered = X - mu
+
+    cov = (X_centered.T @ X_centered) / (N - 1)
+    return cov
